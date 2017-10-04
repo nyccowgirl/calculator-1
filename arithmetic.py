e378aebc7c97db9657c952dee3dea3c5501efc37
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 12:44:44 -0700

    includes further study

--- arithmetic.py
+++ arithmetic.py
@@ -41,3 +41,13 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """ Return the sum of two inputs, and multiplies it with a third input."""
+
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """ Return the sum of the cube of two inputs."""
+
+    return (num1 ** 3) + (num2 ** 3)
